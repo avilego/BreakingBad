7712b3c2fd87d2c212614edddd14caed2799b398
avilego <menavirdgz@gmail.com>
2020-08-20 12:43:02 -0500

    Updates

---
+++ precache-manifest.9db2208f60c0f4d399d7451d58f09744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aac6879ffd4614914f481a48fca9ae4",
+    "url": "/BreakingBad/index.html"
+  },
+  {
+    "revision": "b70e01a5cda7ff1c36fc",
+    "url": "/BreakingBad/static/css/main.25fc78b3.chunk.css"
+  },
+  {
+    "revision": "ced02424512d2040331e",
+    "url": "/BreakingBad/static/js/2.8e0cdecf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BreakingBad/static/js/2.8e0cdecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70e01a5cda7ff1c36fc",
+    "url": "/BreakingBad/static/js/main.5cbcff3b.chunk.js"
+  },
+  {
+    "revision": "a0cf903a0f1901d038a4",
+    "url": "/BreakingBad/static/js/runtime-main.7363abd8.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/BreakingBad/static/media/logo.427a4adc.svg"
+  }
+]);
